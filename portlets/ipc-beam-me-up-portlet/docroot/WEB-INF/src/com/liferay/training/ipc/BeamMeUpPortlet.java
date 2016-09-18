26aeeb46ec658c13e1c270b6d47f1964d77a7865
Rafael Barros <rafaelbarros.df@gmail.com>
2016-09-18 00:57:39 -0300

    Implementing method beamMe

--- portlets/ipc-beam-me-up-portlet/docroot/WEB-INF/src/com/liferay/training/ipc/BeamMeUpPortlet.java
+++ portlets/ipc-beam-me-up-portlet/docroot/WEB-INF/src/com/liferay/training/ipc/BeamMeUpPortlet.java
@@ -4,12 +4,17 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 
 import java.io.IOException;
+import java.util.Random;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.GenericPortlet;
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequestDispatcher;
+import javax.portlet.ProcessAction;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import javax.xml.namespace.QName;
 
 /**
  * Portlet implementation class BeamMeUpPortlet
@@ -17,14 +22,14 @@ import javax.portlet.RenderResponse;
 public class BeamMeUpPortlet extends GenericPortlet {
 
     public void init() {
-        viewTemplate = getInitParameter("view-template");
+        viewJSP = getInitParameter("view-template");
     }
 
     public void doView(
             RenderRequest renderRequest, RenderResponse renderResponse)
         throws IOException, PortletException {
 
-        include(viewTemplate, renderRequest, renderResponse);
+        include(viewJSP, renderRequest, renderResponse);
     }
 
     protected void include(
@@ -42,8 +47,32 @@ public class BeamMeUpPortlet extends GenericPortlet {
             portletRequestDispatcher.include(renderRequest, renderResponse);
         }
     }
+    
+    @ProcessAction(name = "beamMe")
+  	public void beamMe(ActionRequest request, ActionResponse response) {
+  		String destinationType = null;
+  		// Send an Event that the ball has been pitched.
+  		Random random = new Random(System.currentTimeMillis());
+  		int transport = random.nextInt(3) + 1;
+  		switch (transport) {
+  		case 1:
+  			destinationType = "Landing on the planet xyz";
+  			break;
+  		case 2:
+  			destinationType = "Landing on the moon";
+  			break;
+  		case 3:
+  			destinationType = "Landing on the earth";
+  			break;
+  		// should never print
+  		default:
+  			destinationType = "Landing on the spaceship";
+  		}
+  		QName qName = new QName("http://liferay.com/events", "ipc.beammeup");
+  		response.setEvent(qName, destinationType);
+  	}
  
-    protected String viewTemplate;
+    protected String viewJSP;
 
     private static Log _log = LogFactoryUtil.getLog(BeamMeUpPortlet.class);
 
