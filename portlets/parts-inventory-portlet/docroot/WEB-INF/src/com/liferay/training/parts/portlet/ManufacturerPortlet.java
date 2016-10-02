16d1fb70e0fb2d9b350da8968c9d4faa67bb460e
Rafael Barros <rafaelbarros.df@gmail.com>
2016-10-02 13:34:15 -0300

    Creating parts-inventory-portlet

---
+++ portlets/parts-inventory-portlet/docroot/WEB-INF/src/com/liferay/training/parts/portlet/ManufacturerPortlet.java
@@ -0,0 +1,11 @@
+package com.liferay.training.parts.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ManufacturerPortlet
+ */
+public class ManufacturerPortlet extends MVCPortlet {
+ 
+
+}
---
+++ portlets/parts-inventory-portlet/docroot/WEB-INF/src/com/liferay/training/parts/portlet/PartsPortlet.java
@@ -0,0 +1,11 @@
+package com.liferay.training.parts.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class PartsPortlet
+ */
+public class PartsPortlet extends MVCPortlet {
+ 
+
+}
