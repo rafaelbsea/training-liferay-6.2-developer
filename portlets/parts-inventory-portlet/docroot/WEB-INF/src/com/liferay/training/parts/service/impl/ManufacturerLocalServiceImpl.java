2cc262f6391d603706ef2a8a1a2edba62fad3837
Rafael Barros <rafael.silva@seatecnologia.com.br>
2016-10-12 01:07:59 -0300

    Implementing the manufacturer logic

--- portlets/parts-inventory-portlet/docroot/WEB-INF/src/com/liferay/training/parts/service/impl/ManufacturerLocalServiceImpl.java
+++ portlets/parts-inventory-portlet/docroot/WEB-INF/src/com/liferay/training/parts/service/impl/ManufacturerLocalServiceImpl.java
@@ -14,27 +14,123 @@
 
 package com.liferay.training.parts.service.impl;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.ResourceConstants;
+import com.liferay.training.parts.model.Manufacturer;
 import com.liferay.training.parts.service.base.ManufacturerLocalServiceBaseImpl;
+
+import java.util.List;
 
 /**
  * The implementation of the manufacturer local service.
  *
  * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.training.parts.service.ManufacturerLocalService} interface.
+ * All custom service methods should be put in this class. Whenever methods are
+ * added, rerun ServiceBuilder to copy their definitions into the
+ * {@link com.liferay.training.parts.service.ManufacturerLocalService}
+ * interface.
  *
  * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * This is a local service. Methods of this service will not have security
+ * checks based on the propagated JAAS credentials because this service can only
+ * be accessed from within the same VM.
  * </p>
  *
  * @author rafael barros
  * @see com.liferay.training.parts.service.base.ManufacturerLocalServiceBaseImpl
  * @see com.liferay.training.parts.service.ManufacturerLocalServiceUtil
  */
-public class ManufacturerLocalServiceImpl
-	extends ManufacturerLocalServiceBaseImpl {
+public class ManufacturerLocalServiceImpl extends
+		ManufacturerLocalServiceBaseImpl {
 	/*
 	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this interface directly. Always use {@link com.liferay.training.parts.service.ManufacturerLocalServiceUtil} to access the manufacturer local service.
+	 * 
+	 * Never reference this interface directly. Always use {@link
+	 * com.liferay.training.parts.service.ManufacturerLocalServiceUtil} to
+	 * access the manufacturer local service.
 	 */
+
+	/**
+	 * Adds the Manufacturer to the database incrementing the primary key
+	 * 
+	 * @throws PortalException
+	 * 
+	 */
+
+	public Manufacturer addManufacturer(Manufacturer newManufacturer,
+			long userId) throws SystemException, PortalException {
+
+		long manufacturerId = counterLocalService.increment(Manufacturer.class
+				.getName());
+
+		Manufacturer manufacturer = manufacturerPersistence
+				.create(manufacturerId);
+		manufacturer.setCompanyId(newManufacturer.getCompanyId());
+		manufacturer.setEmailAddress(newManufacturer.getEmailAddress());
+		manufacturer.setGroupId(newManufacturer.getGroupId());
+		manufacturer.setName(newManufacturer.getName());
+		manufacturer.setPhoneNumber(newManufacturer.getPhoneNumber());
+		manufacturer.setWebsite(newManufacturer.getWebsite());
+
+		manufacturerPersistence.update(manufacturer);
+		return manufacturer;
+	}
+
+	/**
+	 * Deletes a manufacturer from the database using the Manufacturer object.
+	 */
+	public Manufacturer deleteManufacturer(Manufacturer manufacturer)
+			throws SystemException {
+
+		try {
+			resourceLocalService.deleteResource(manufacturer.getCompanyId(),
+					Manufacturer.class.getName(),
+					ResourceConstants.SCOPE_INDIVIDUAL,
+					manufacturer.getPrimaryKey());
+		} catch (PortalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return super.deleteManufacturer(manufacturer);
+	}
+
+	/**
+	 * Deletes a manufacturer from the database using a manufacturer ID.
+	 */
+	public Manufacturer deleteManufacturer(long manufacturerId)
+			throws PortalException, SystemException {
+
+		Manufacturer manufacturer = getManufacturer(manufacturerId);
+
+		return deleteManufacturer(manufacturer);
+	}
+
+	/**
+	 * Gets a list with all the Manufacturers in a group
+	 *
+	 */
+	public List<Manufacturer> getManufacturersByGroupId(long groupId)
+			throws SystemException {
+		return manufacturerPersistence.findByGroupId(groupId);
+	}
+
+	/**
+	 * Gets a list with a range of Manufacturers from a group
+	 *
+	 */
+	public List<Manufacturer> getManufacturersByGroupId(long groupId,
+			int start, int end) throws SystemException {
+		return manufacturerPersistence.findByGroupId(groupId, start, end);
+	}
+
+	/**
+	 * Gets the number of Manufacturers in a group
+	 *
+	 */
+	public int getManufacturersCountByGroupId(long groupId)
+			throws SystemException {
+		return manufacturerPersistence.countByGroupId(groupId);
+	}
 }
