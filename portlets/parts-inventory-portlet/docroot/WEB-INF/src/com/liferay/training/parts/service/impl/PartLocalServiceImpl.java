d3a582ac2b54995fd7cc3550943713b02063c4ff
Rafael Barros <rafael.silva@seatecnologia.com.br>
2016-10-12 01:13:45 -0300

    Implementing the part logic

--- portlets/parts-inventory-portlet/docroot/WEB-INF/src/com/liferay/training/parts/service/impl/PartLocalServiceImpl.java
+++ portlets/parts-inventory-portlet/docroot/WEB-INF/src/com/liferay/training/parts/service/impl/PartLocalServiceImpl.java
@@ -14,16 +14,26 @@
 
 package com.liferay.training.parts.service.impl;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.ResourceConstants;
+import com.liferay.training.parts.model.Part;
 import com.liferay.training.parts.service.base.PartLocalServiceBaseImpl;
+
+import java.util.List;
 
 /**
  * The implementation of the part local service.
  *
  * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.training.parts.service.PartLocalService} interface.
+ * All custom service methods should be put in this class. Whenever methods are
+ * added, rerun ServiceBuilder to copy their definitions into the
+ * {@link com.liferay.training.parts.service.PartLocalService} interface.
  *
  * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * This is a local service. Methods of this service will not have security
+ * checks based on the propagated JAAS credentials because this service can only
+ * be accessed from within the same VM.
  * </p>
  *
  * @author rafael barros
@@ -33,7 +43,90 @@ import com.liferay.training.parts.service.base.PartLocalServiceBaseImpl;
 public class PartLocalServiceImpl extends PartLocalServiceBaseImpl {
 	/*
 	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this interface directly. Always use {@link com.liferay.training.parts.service.PartLocalServiceUtil} to access the part local service.
+	 * 
+	 * Never reference this interface directly. Always use {@link
+	 * com.liferay.training.parts.service.PartLocalServiceUtil} to access the
+	 * part local service.
 	 */
+
+	/**
+	 * Adds the Part to the database incrementing the primary key
+	 * 
+	 * @throws PortalException
+	 * 
+	 */
+	public Part addPart(Part newPart, long userId) throws SystemException,
+			PortalException {
+		long partId = counterLocalService.increment(Part.class.getName());
+
+		Part part = partPersistence.create(partId);
+		part.setPartNumber(newPart.getPartNumber());
+		part.setCompanyId(newPart.getCompanyId());
+		part.setGroupId(newPart.getGroupId());
+		part.setOrderDate(newPart.getOrderDate());
+		part.setQuantity(newPart.getQuantity());
+		part.setManufacturerId(newPart.getManufacturerId());
+		part.setName(newPart.getName());
+
+		partPersistence.update(part);
+
+		return part;
+
+	}
+
+	public Part deletePart(Part part) throws SystemException {
+
+		try {
+			resourceLocalService.deleteResource(part.getCompanyId(),
+					Part.class.getName(), ResourceConstants.SCOPE_INDIVIDUAL,
+					part.getPrimaryKey());
+		} catch (PortalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return super.deletePart(part);
+
+	}
+
+	public Part deletePart(long partId) throws PortalException, SystemException {
+
+		Part part = partLocalService.getPart(partId);
+
+		return deletePart(part);
+	}
+
+	/**
+	 * Gets a list with all the Parts in a group
+	 *
+	 */
+	public List<Part> getPartsByGroupId(long groupId) throws SystemException {
+		return partPersistence.findByGroupId(groupId);
+	}
+
+	/**
+	 * Gets a list with a range of Parts from a group
+	 *
+	 */
+	public List<Part> getPartsByGroupId(long groupId, int start, int end)
+			throws SystemException {
+		return partPersistence.findByGroupId(groupId, start, end);
+	}
+
+	/**
+	 * Gets the number of Parts in a group
+	 *
+	 */
+	public int getPartsCountByGroupId(long groupId) throws SystemException {
+		return partPersistence.countByGroupId(groupId);
+	}
+
+	/**
+	 * Gets a list of Parts from a Manufacturer
+	 *
+	 */
+	public List<Part> getPartsbyManufacturer(long manufacturerId, long groupId)
+			throws SystemException {
+		return partPersistence.findByManufacturer(manufacturerId, groupId);
+	}
 }
