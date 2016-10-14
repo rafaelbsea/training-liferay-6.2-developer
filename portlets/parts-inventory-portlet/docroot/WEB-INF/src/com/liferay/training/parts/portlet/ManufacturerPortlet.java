1773cdb221da4d503076eb1c0bed7bc5ad8d9427
Rafael Barros <rafael.silva@seatecnologia.com.br>
2016-10-14 00:21:26 -0300

    Implementing action addManufacturer

--- portlets/parts-inventory-portlet/docroot/WEB-INF/src/com/liferay/training/parts/portlet/ManufacturerPortlet.java
+++ portlets/parts-inventory-portlet/docroot/WEB-INF/src/com/liferay/training/parts/portlet/ManufacturerPortlet.java
@@ -1,11 +1,66 @@
 package com.liferay.training.parts.portlet;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.training.parts.model.Manufacturer;
+import com.liferay.training.parts.model.impl.ManufacturerImpl;
+import com.liferay.training.parts.service.ManufacturerLocalServiceUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletRequest;
 
 /**
  * Portlet implementation class ManufacturerPortlet
  */
 public class ManufacturerPortlet extends MVCPortlet {
  
+	/**
+	 * Adds a new manufacturer to the database
+	 * 
+	 */
+	public void addManufacturer(ActionRequest request, ActionResponse response)
+			throws Exception {
 
+		ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+		
+		Manufacturer manufacturer = manufacturerFromRequest(request);
+
+		long userId = themeDisplay.getUserId();
+		ManufacturerLocalServiceUtil.addManufacturer(manufacturer, userId);
+		SessionMessages.add(request, "manufacturer-added");
+		sendRedirect(request, response);
+
+	}
+	
+	
+	/**
+	 * Convenience method to create a Manufacturer object out of the request.
+	 * Used by the Add / Edit methods.
+	 * 
+	 */
+	private Manufacturer manufacturerFromRequest(PortletRequest request) {
+		ThemeDisplay themeDisplay = (ThemeDisplay) request
+				.getAttribute(WebKeys.THEME_DISPLAY);
+
+		ManufacturerImpl manufacturer = new ManufacturerImpl();
+
+		manufacturer.setManufacturerId(ParamUtil.getLong(request,"manufacturerId"));
+		manufacturer.setName(ParamUtil.getString(request, "name"));
+		manufacturer.setEmailAddress(ParamUtil.getString(request,"emailAddress"));
+		manufacturer.setWebsite(ParamUtil.getString(request, "website"));
+		manufacturer.setPhoneNumber(ParamUtil.getString(request, "phoneNumber"));
+		manufacturer.setCompanyId(themeDisplay.getCompanyId());
+		manufacturer.setGroupId(themeDisplay.getScopeGroupId());
+		manufacturer.setUserId(themeDisplay.getUserId());
+
+		return manufacturer;
+	}
+	
+	private static Log _log = LogFactoryUtil.getLog(ManufacturerPortlet.class);
 }
